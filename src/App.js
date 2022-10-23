434d53302514b13c6b2034e39d25daa3a1794f9c
RoboTeemo <52300830+RoboTeemo@users.noreply.github.com>
2022-10-23 21:23:35 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <News/>
+      {/* <News/>
       <ProductList title={'Best Price'}/>
-      <div style={{textAlign: 'center', margin: '1rem 30rem 2rem 0'}}>
+      <div style={{
+        textAlign: 'center', 
+        margin: '1rem 30rem 2rem 0', 
+        
+        }}>
         * weight of just cooked product. The weight in delivery orders may differ due to dehydration of the product.
       </div>
-      <Footer/>
+      <Footer/> */}
     </div>
   );
 }
